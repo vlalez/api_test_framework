e070d6121b8adc061afa0734502e115d7d1b14b0
VLezhnenko <vlalez@icloud.com>
2026-05-15 14:16:39 +0200

    Added configuration file

---
+++ config/config.py
@@ -0,0 +1,12 @@
+import os
+
+
+ENVIRONMENTS = {
+    "dev": "https://jsonplaceholder.typicode.com",
+    "test": "https://jsonplaceholder.typicode.com",
+    "prod": "https://jsonplaceholder.typicode.com"
+
+}
+
+TEST_ENV = os.getenv("TEST_ENV","test")
+base_url = ENVIRONMENTS[TEST_ENV]
